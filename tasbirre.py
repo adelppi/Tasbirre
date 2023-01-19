6f55a9889e403516fb55e43f6a825aa6e97204b2
adelppi <adelppi.baig@gmail.com>
2023-01-20 01:37:01 +0900

    wip: image crop issue: it loads image every loop

--- tasbirre.py
+++ tasbirre.py
@@ -52,7 +52,7 @@ editPart = sg.Frame(
             sg.T("明るさ", size = (8, 0), justification = "right", font = ("Meiryo UI", 15)),
             sg.Slider(
                 range = (-5, 5),
-                default_value = 0,
+                default_value = 1,
                 resolution = 0.01,
                 orientation = "h",
                 key = "sliderBrightness"
@@ -91,6 +91,10 @@ layout = [
 window = sg.Window("Tasbirre", layout, size = (windowSizeX, windowSizeY), resizable = True)
 window.Finalize()
 window["imageRawPreview"].bind("<Button1-Motion>", "__DRAG")
+window["imageRawPreview"].bind("<Button1-ButtonRelease>", "__RELEASE")
+window["imageRawPreview"].bind("<Button1-ButtonPress>", "__PRESS")
+widget = window["imageRawPreview"].Widget
+dragXY = []
 
 while True:
     event, values = window.read(timeout = 0)
@@ -110,8 +114,11 @@ while True:
         rawImage = getImageData(imagePath) if os.path.exists(imagePath) else blankImage
         window["imageRawPreview"].update(data = rawImage.tobytes())
         editImage = cv2.resize(cv2.imread(imagePath), dsize = None, fx = imageRatioX, fy = imageRatioY) if os.path.exists(imagePath) else rawImage.copy()
+        if event == "imageRawPreview__RELEASE":
+            editImage = editImage[cropStart[0]:cropEnd[0], cropStart[1]:cropEnd[1]]
+            dragXY = []
+            print(cropStart, cropEnd)
         editImage = cv2.LUT(editImage, resultLUT)
-
         resultImage = cv2.imencode('.png', editImage)[1]
         window["imageResultPreview"].update(data = resultImage.tobytes())
 
@@ -124,7 +131,11 @@ while True:
         window["after"].update("編集後:")
 
     if event == "imageRawPreview__DRAG":
-        print("dragged")
+        dragXY.append([widget.winfo_pointerx() - widget.winfo_rootx(),
+                       widget.winfo_pointery() - widget.winfo_rooty()])
+        cropStart = dragXY[0]
+        cropEnd = dragXY[-1]
+        
 
     if event == "buttonRevert":
         window["sliderBrightness"].update(0)
